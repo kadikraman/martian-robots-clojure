d7ae73218c9234d10c51b797b2a421f33545bd24
Kadi Kraman <kadi.kraman@red-badger.com>
2017-03-09 17:06:20 +0100

    Add a rotate function

--- src/martian_robots_clojure/core.clj
+++ src/martian_robots_clojure/core.clj
@@ -3,10 +3,35 @@
 (defn move
   [robot]
   (cond
-    (= (:orientation robot) :N) (update-in robot [:y] inc)
-    (= (:orientation robot) :S) (update-in robot [:y] dec)
-    (= (:orientation robot) :E) (update-in robot [:x] inc)
-    (= (:orientation robot) :W) (update-in robot [:x] dec)))
+    (= (:orientation robot) :N) (update robot :y inc)
+    (= (:orientation robot) :S) (update robot :y dec)
+    (= (:orientation robot) :E) (update robot :x inc)
+    (= (:orientation robot) :W) (update robot :x dec)))
+
+
+(defn rotate-right
+  [robot]
+  (cond
+    (= (:orientation robot) :N) (assoc robot :orientation :E)
+    (= (:orientation robot) :E) (assoc robot :orientation :S)
+    (= (:orientation robot) :S) (assoc robot :orientation :W)
+    (= (:orientation robot) :W) (assoc robot :orientation :N)))
+
+
+(defn rotate-left
+  [robot]
+  (cond
+    (= (:orientation robot) :N) (assoc robot :orientation :W)
+    (= (:orientation robot) :W) (assoc robot :orientation :S)
+    (= (:orientation robot) :S) (assoc robot :orientation :E)
+    (= (:orientation robot) :E) (assoc robot :orientation :N)))
+
+(defn rotate
+  [robot direction]
+  (cond
+    (= direction :L) (rotate-left robot)
+    (= direction :R) (rotate-right robot)))
+
 
 (defn -main
   []
--- test/martian_robots_clojure/core_test.clj
+++ test/martian_robots_clojure/core_test.clj
@@ -11,3 +11,21 @@
     (is (= (move {:x 1 :y 2 :orientation :E }) {:x 2 :y 2 :orientation :E})))
   (testing "Robot moves correctly when oriented West"
     (is (= (move {:x 1 :y 2 :orientation :W }) {:x 0 :y 2 :orientation :W}))))
+
+(deftest rotate-test
+  (testing "Robot turns right correctly when facing north"
+    (is (= (rotate {:x 1 :y 2 :orientation :N } :R) {:x 1 :y 2 :orientation :E})))
+  (testing "Robot turns left correctly when facing north"
+    (is (= (rotate {:x 1 :y 2 :orientation :N } :L) {:x 1 :y 2 :orientation :W})))
+  (testing "Robot turns right correctly when facing south"
+    (is (= (rotate {:x 1 :y 2 :orientation :S } :R) {:x 1 :y 2 :orientation :W})))
+  (testing "Robot turns left correctly when facing south"
+    (is (= (rotate {:x 1 :y 2 :orientation :S } :L) {:x 1 :y 2 :orientation :E})))
+  (testing "Robot turns right correctly when facing east"
+    (is (= (rotate {:x 1 :y 2 :orientation :E } :R) {:x 1 :y 2 :orientation :S})))
+  (testing "Robot turns left correctly when facing east"
+    (is (= (rotate {:x 1 :y 2 :orientation :E } :L) {:x 1 :y 2 :orientation :N})))
+  (testing "Robot turns right correctly when facing west"
+    (is (= (rotate {:x 1 :y 2 :orientation :W } :R) {:x 1 :y 2 :orientation :N})))
+  (testing "Robot turns left correctly when facing west"
+    (is (= (rotate {:x 1 :y 2 :orientation :W } :L) {:x 1 :y 2 :orientation :S}))))
