58cc61a2b45e6b9e944606ef8b889c9f114d32f1
Kadi Kraman <kadi.kraman@red-badger.com>
2017-03-09 16:33:41 +0100

    Add move function

--- src/martian_robots_clojure/core.clj
+++ src/martian_robots_clojure/core.clj
@@ -1,6 +1,13 @@
 (ns martian-robots-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn move
+  [robot]
+  (cond
+    (= (:orientation robot) :N) (update-in robot [:y] inc)
+    (= (:orientation robot) :S) (update-in robot [:y] dec)
+    (= (:orientation robot) :E) (update-in robot [:x] inc)
+    (= (:orientation robot) :W) (update-in robot [:x] dec)))
+
+(defn -main
+  []
+  (println (move {:x 1 :y 2 :orientation :W})))
--- test/martian_robots_clojure/core_test.clj
+++ test/martian_robots_clojure/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [martian-robots-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest move-test
+  (testing "Robot moves correctly when oriented North"
+    (is (= (move {:x 1 :y 2 :orientation :N }) {:x 1 :y 3 :orientation :N})))
+  (testing "Robot moves correctly when oriented South"
+    (is (= (move {:x 1 :y 2 :orientation :S }) {:x 1 :y 1 :orientation :S})))
+  (testing "Robot moves correctly when oriented East"
+    (is (= (move {:x 1 :y 2 :orientation :E }) {:x 2 :y 2 :orientation :E})))
+  (testing "Robot moves correctly when oriented West"
+    (is (= (move {:x 1 :y 2 :orientation :W }) {:x 0 :y 2 :orientation :W}))))
