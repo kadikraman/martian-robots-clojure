da3d9c17e7c4f7976024f793f6dddc5aa0810a7c
Kadi Kraman <kadi.kraman@red-badger.com>
2017-03-09 15:18:48 +0100

    Initial commit

---
+++ test/martian_robots_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns martian-robots-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [martian-robots-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
